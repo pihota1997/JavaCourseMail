dbc1e853fd8c7284f5a071a0174dd7dbd0a4ff8b
pihota1997 <pihota1997@yandex.ru>
2021-10-14 11:06:16 +0300

    HomeWork1_library

--- library/controller/src/main/java/Library.java
+++ library/controller/src/main/java/Library.java
@@ -1,3 +1,6 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.*;
 
 public class Library {
@@ -14,5 +17,18 @@ public class Library {
 
     public HashMap<String, String> getBooks() {
         return this.books;
+    }
+
+    public void authorBooks() {
+        final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+        Scanner scanner = new Scanner(System.in);
+        List<String> thisAuthorBooks = new ArrayList<>();
+        String author = scanner.nextLine();
+        for (Map.Entry<String, String> entry : books.entrySet()) {
+            if (author.equals(entry.getValue()))
+                thisAuthorBooks.add(entry.getKey());
+        }
+        String json = GSON.toJson(thisAuthorBooks);
+        System.out.println(json);
     }
 }
